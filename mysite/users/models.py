e552ebe347fc545c6a2e5f5d5820719ab0571b2e
Parin Sopitlaptana <6210612740@student.tu.ac.th>
2022-03-14 13:53:31 +0700

    first db

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,3 +1,15 @@
+from pyexpat import model
+from re import M
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+  
+class UserProfile(models.Model):  
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True) 
+    role = models.CharField(max_length=16)
+    age = models.CharField(max_length=16)
+    bio = models.CharField(max_length=200)
+      
+    

ea08816aac34b565905d4864a65c5268bda53cde
Mc Punyawat <6210612799@student.tu.ac.th>
2022-05-02 00:50:55 +0700

    comment can put and show on update page :zap:

--- mysite/project/urls.py
+++ mysite/project/urls.py
@@ -6,6 +6,7 @@ app_name="project"
 
 urlpatterns = [
     path('create_project', views.ProjectCreateView.as_view(), name='create_project'),
-    path('view_project', views.PreProjectView, name='view_project'),
+    #path('view_project', views.PreProjectView, name='view_project'),
+    path('comment_project', views.CommentCreateView , name='comment_project' ),
 
 ]
